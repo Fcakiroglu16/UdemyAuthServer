24fe164780fbcdd5fde22ff6e1832b7fb87f7159
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-12-20 23:22:44 +0300

    ErrorDto sınıfı oluşturmak

---
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary.Dtos
+{
+    public class ErrorDto
+    {
+        public List<String> Errors { get; private set; }
+
+        public bool IsShow { get; private set; }
+
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+
+        public ErrorDto(string error, bool isShow)
+        {
+            Errors.Add(error);
+            isShow = true;
+        }
+
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = Errors;
+            IsShow = isShow;
+        }
+    }
+}

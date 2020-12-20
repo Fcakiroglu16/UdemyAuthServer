198ae2db9921c467ce2281e99f86474d654e6d96
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-12-20 23:46:31 +0300

    Response<T> sınıfı oluşturmak

---
+++ SharedLibrary/Dtos/Response.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace SharedLibrary.Dtos
+{
+    public class Response<T> where T : class
+    {
+        public T Data { get; private set; }
+        public int StatusCode { get; private set; }
+
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+
+        public ErrorDto Error { get; private set; }
+
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T> { Data = default, StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static Response<T> Fail(ErrorDto errorDto, int statusCode)
+        {
+            return new Response<T>
+            {
+                Error = errorDto,
+                StatusCode = statusCode,
+                IsSuccessful = false
+            };
+        }
+
+        public static Response<T> Fail(string errorMessage, int statusCode, bool isShow)
+        {
+            var errorDto = new ErrorDto(errorMessage, isShow);
+
+            return new Response<T> { Error = errorDto, StatusCode = statusCode, IsSuccessful = false };
+        }
+    }
+}

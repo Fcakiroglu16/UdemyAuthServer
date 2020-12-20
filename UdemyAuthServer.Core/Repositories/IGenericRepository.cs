09e8ef4d71cd00a4349f6b1ea7c712cafcea63d8
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-12-20 22:33:58 +0300

    IRepository interface

---
+++ UdemyAuthServer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyAuthServer.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+
+        Task AddAsync(TEntity entity);
+
+        void Remove(TEntity entity);
+
+        TEntity Update(TEntity entity);
+    }
+}

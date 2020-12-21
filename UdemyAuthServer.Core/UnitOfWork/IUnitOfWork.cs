a548c545d9cdafaedd6b939f5fffc501705f6e70
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-12-21 23:39:44 +0300

    IUnitOfWork interface'sinin eklenmesi

---
+++ UdemyAuthServer.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyAuthServer.Core.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task CommmitAsync();
+
+        void Commit();
+    }
+}

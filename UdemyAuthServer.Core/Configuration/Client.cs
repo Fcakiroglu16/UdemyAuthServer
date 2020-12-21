9ea9432048695d60d8841b99a165880457491d18
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-12-21 22:32:15 +0300

    ITokenService interface'sinin oluşturulması

---
+++ UdemyAuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyAuthServer.Core.Configuration
+{
+    public class Client
+    {
+        public string Id { get; set; }
+
+        public string Secret { get; set; }
+
+        public List<String> Audiences { get; set; }
+    }
+}
---
+++ UdemyAuthServer.Core/Services/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UdemyAuthServer.Core.Configuration;
+using UdemyAuthServer.Core.DTOs;
+using UdemyAuthServer.Core.Models;
+
+namespace UdemyAuthServer.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}

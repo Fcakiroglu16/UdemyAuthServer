390fbc36258f9559c7eb3d1066e7a1d38d3bdd44
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-12-22 00:01:36 +0300

    AppDbContext class'sının oluşturulması

---
+++ UdemyAuthServer.Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UdemyAuthServer.Core.Models;
+
+namespace UdemyAuthServer.Data
+{
+    // Identity üyelik tablolar
+    //
+
+    public class AppDbContext : IdentityDbContext<UserApp, IdentityRole, string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+
+        {
+        }
+
+        public DbSet<Product> Produts { get; set; }
+
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- UdemyAuthServer.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace UdemyAuthServer.Data
-{
-    public class Class1
-    {
-    }
-}

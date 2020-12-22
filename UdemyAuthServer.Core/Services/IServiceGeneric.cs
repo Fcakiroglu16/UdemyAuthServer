190acbdc02a441b8953a03b7835c178b95156d6e
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-12-23 00:41:07 +0300

    GenericService  class’sının oluşturulması

--- UdemyAuthServer.Core/Services/IServiceGeneric.cs
+++ UdemyAuthServer.Core/Services/IServiceGeneric.cs
@@ -15,10 +15,10 @@ namespace UdemyAuthServer.Core.Services
 
         Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate);
 
-        Task<Response<TDto>> AddAsync(TEntity entity);
+        Task<Response<TDto>> AddAsync(TDto entity);
 
-        Task<Response<NoDataDto>> Remove(TEntity entity);
+        Task<Response<NoDataDto>> Remove(int id);
 
-        Task<Response<NoDataDto>> Update(TEntity entity);
+        Task<Response<NoDataDto>> Update(TDto entity, int id);
     }
 }
---
+++ UdemyAuthServer.Service/Services/ServiceGeneric.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.Repositories;
+using UdemyAuthServer.Core.Services;
+using UdemyAuthServer.Core.UnitOfWork;
+
+namespace UdemyAuthServer.Service.Services
+{
+    public class ServiceGeneric<TEntity, TDto> : IServiceGeneric<TEntity, TDto> where TEntity : class where TDto : class
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        private readonly IGenericRepository<TEntity> _genericRepository;
+
+        public ServiceGeneric(IUnitOfWork unitOfWork, IGenericRepository<TEntity> genericRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _genericRepository = genericRepository;
+        }
+
+        public async Task<Response<TDto>> AddAsync(TDto entity)
+        {
+            var newEntity = ObjectMapper.Mapper.Map<TEntity>(entity);
+
+            await _genericRepository.AddAsync(newEntity);
+
+            await _unitOfWork.CommmitAsync();
+
+            var newDto = ObjectMapper.Mapper.Map<TDto>(newEntity);
+
+            return Response<TDto>.Success(newDto, 200);
+        }
+
+        public async Task<Response<IEnumerable<TDto>>> GetAllAsync()
+        {
+            var products = ObjectMapper.Mapper.Map<List<TDto>>(await _genericRepository.GetAllAsync());
+
+            return Response<IEnumerable<TDto>>.Success(products, 200);
+        }
+
+        public async Task<Response<TDto>> GetByIdAsync(int id)
+        {
+            var product = await _genericRepository.GetByIdAsync(id);
+
+            if (product == null)
+            {
+                return Response<TDto>.Fail("Id not found", 404, true);
+            }
+
+            return Response<TDto>.Success(ObjectMapper.Mapper.Map<TDto>(product), 200);
+        }
+
+        public async Task<Response<NoDataDto>> Remove(int id)
+        {
+            var isExistEntity = await _genericRepository.GetByIdAsync(id);
+
+            if (isExistEntity == null)
+            {
+                return Response<NoDataDto>.Fail("Id not found", 404, true);
+            }
+
+            _genericRepository.Remove(isExistEntity);
+
+            await _unitOfWork.CommmitAsync();
+            //204 durum kodu =>  No Content  => Response body'sinde hiç bir dat  olmayacak.
+            return Response<NoDataDto>.Success(204);
+        }
+
+        public async Task<Response<NoDataDto>> Update(TDto entity, int id)
+        {
+            var isExistEntity = await _genericRepository.GetByIdAsync(id);
+
+            if (isExistEntity == null)
+            {
+                return Response<NoDataDto>.Fail("Id not found", 404, true);
+            }
+
+            var updateEntity = ObjectMapper.Mapper.Map<TEntity>(entity);
+
+            _genericRepository.Update(updateEntity);
+
+            await _unitOfWork.CommmitAsync();
+            //204 durum kodu =>  No Content  => Response body'sinde hiç bir dat  olmayacak.
+            return Response<NoDataDto>.Success(204);
+        }
+
+        public async Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate)
+        {
+            // where(x=>x.id>5)
+            var list = _genericRepository.Where(predicate);
+
+            return Response<IEnumerable<TDto>>.Success(ObjectMapper.Mapper.Map<IEnumerable<TDto>>(await list.ToListAsync()), 200);
+        }
+    }
+}

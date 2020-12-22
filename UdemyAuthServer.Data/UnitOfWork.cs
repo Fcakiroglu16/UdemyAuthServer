a04dc18b57b98f59bcc7603b86b0a378d3328da4
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-12-22 23:18:10 +0300

    UnitOfWork  class’sının oluşturulması

---
+++ UdemyAuthServer.Data/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.UnitOfWork;
+
+namespace UdemyAuthServer.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommmitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}

04fa53cb46e7e684179dd4fb9180a11955acf2a5
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-12-22 22:56:48 +0300

    GenericRepository  class’sının oluşturulması

---
+++ UdemyAuthServer.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.Repositories;
+
+namespace UdemyAuthServer.Data.Repositories
+{
+    public class GenericRepository<Tentity> : IGenericRepository<Tentity> where Tentity : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<Tentity> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<Tentity>();
+        }
+
+        public async Task AddAsync(Tentity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<Tentity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<Tentity> GetByIdAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            // EntityState.Detached yapısını service class'sını anlatırken detaylandıracağım.
+            if (entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+
+            return entity;
+        }
+
+        public void Remove(Tentity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public Tentity Update(Tentity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+
+            return entity;
+        }
+
+        public IQueryable<Tentity> Where(Expression<Func<Tentity, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+    }
+}

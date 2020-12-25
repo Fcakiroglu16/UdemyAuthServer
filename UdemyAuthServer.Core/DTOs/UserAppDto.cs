4d992fd244db2ee639ffba39c48da0ab7c6e0db1
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-12-25 05:57:33 +0300

    Postman Endpoint test işlemi

--- UdemyAuthServer.Core/DTOs/UserAppDto.cs
+++ UdemyAuthServer.Core/DTOs/UserAppDto.cs
@@ -6,7 +6,7 @@ namespace UdemyAuthServer.Core.DTOs
 {
     public class UserAppDto
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
 

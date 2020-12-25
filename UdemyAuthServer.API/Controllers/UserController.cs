8f92e2a9e92084fb149e5ef19d3dbd3dc3e8ba9d
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-12-25 05:34:32 +0300

    UserController sınıfının oluşturulması

---
+++ UdemyAuthServer.API/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.DTOs;
+using UdemyAuthServer.Core.Services;
+
+namespace UdemyAuthServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : CustomBaseController
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        //api/user
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserDto createUserDto)
+        {
+            return ActionResultInstance(await _userService.CreateUserAsync(createUserDto));
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            return ActionResultInstance(await _userService.GetUserByNameAsync(HttpContext.User.Identity.Name));
+        }
+    }
+}
--- UdemyAuthServer.API/Startup.cs
+++ UdemyAuthServer.API/Startup.cs
@@ -104,7 +104,7 @@ namespace UdemyAuthServer.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

0f848ee64a5bcdd82da5e3c8a7bf8bb67196fd38
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-12-27 19:26:46 +0300

    Mini API’lerimiz için controller oluşturma

---
+++ MiniApp1.API/Controllers/StockController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MiniApp1.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        public IActionResult GetStock()
+        {
+            var userName = HttpContext.User.Identity.Name;
+
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+            //veri tabanında  userId veya userName alanları üzerinden gerekli dataları çek
+
+            // stockId stockQuantity  Category  UserId/UserName
+
+            return Ok($"Stock işlemleri  =>UserName: {userName }- UserId:{userId}");
+        }
+    }
+}
--- MiniApp3.API/Controllers/WeatherForecastController.cs
+++ MiniApp3.API/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace MiniApp3.API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
